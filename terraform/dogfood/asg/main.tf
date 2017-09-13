b1f6a8b9592b31c822af59c79febdbd5168f2f1d
Tim Koopmans <tim.koops@gmail.com>
2017-09-13 10:26:00 +0200

    adding dogfood API

---
+++ terraform/dogfood/asg/main.tf
@@ -0,0 +1,40 @@
+variable "profile" {}
+variable "region" {}
+variable "shared_credentials_file" {}
+
+variable "dd_api_key" {}
+variable "image_id" {}
+variable "instance_type" {}
+variable "asg_size" { default = "1" }
+
+provider "aws" {
+  shared_credentials_file = "${var.shared_credentials_file}"
+  profile = "${var.profile}"
+  region = "${var.region}"
+}
+
+resource "aws_launch_configuration" "dogfood-launch-config" {
+  name = "dogfood-launch-config"
+  image_id =  "${var.image_id}"
+  instance_type = "${var.instance_type}"
+  user_data = "${replace(file("cloudconfig.yml"), "DD_API_KEY", "${var.dd_api_key}")}"
+  security_groups = ["allow_all_for_dogfood"]
+}
+
+resource "aws_autoscaling_group" "dogfood-asg" {
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  name = "dogfood-asg"
+  max_size = "${var.asg_size}"
+  min_size = "${var.asg_size}"
+  health_check_grace_period = 600
+  health_check_type = "ELB"
+  desired_capacity = "${var.asg_size}"
+  force_delete = true
+  launch_configuration = "${aws_launch_configuration.dogfood-launch-config.name}"
+  load_balancers = ["dogfood-elb"]
+  tag {
+    key = "Name"
+    value = "dogfood"
+    propagate_at_launch = true
+  }
+}
---
+++ terraform/dogfood/elb/main.tf
@@ -0,0 +1,76 @@
+variable "profile" {}
+variable "region" {}
+variable "shared_credentials_file" {}
+
+provider "aws" {
+  shared_credentials_file = "${var.shared_credentials_file}"
+  profile = "${var.profile}"
+  region = "${var.region}"
+}
+
+resource "aws_security_group" "dogfood-sg" {
+  name = "allow_all_for_dogfood"
+  description = "Allow all web traffic"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    self = true
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    self = true
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    self = true
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    self = true
+  }
+}
+
+resource "aws_elb" "dogfood-elb" {
+  name = "dogfood-elb"
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  security_groups = ["${aws_security_group.dogfood-sg.id}"]
+
+  listener {
+    instance_port = 8080
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:8080/"
+    interval = 30
+  }
+
+  cross_zone_load_balancing = true
+  idle_timeout = 400
+  connection_draining = true
+  connection_draining_timeout = 400
+}
+
+output "dns_name" {
+  value = "${aws_elb.dogfood-elb.dns_name}"
+}
