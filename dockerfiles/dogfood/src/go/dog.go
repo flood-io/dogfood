f345ee8e749fd4d8a95b92d61df7cbb4226a8407
Tim Koopmans <tim.koops@gmail.com>
2017-09-15 12:27:02 +0200

    working on responses

--- dockerfiles/dogfood/src/go/dog.go
+++ dockerfiles/dogfood/src/go/dog.go
@@ -3,14 +3,25 @@ package dogfood
 import (
 	"fmt"
 	"net/http"
+	"time"
+	"encoding/json"
+	"github.com/Pallinder/sillyname-go"
 )
 
 type Dog struct {
-
+	Name string
+  Body string
+  Time int32
 }
 
 func Adddog(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		m := Dog{sillyname.GenerateStupidName(), "Hello", int32(time.Now().Unix())}
+		b, err := json.Marshal(m)
+		if err != nil {
+      panic(err)
+    }
+		fmt.Fprintf(w, string(b))
 		w.WriteHeader(http.StatusOK)
 }
 
@@ -21,32 +32,67 @@ func Deletedog(w http.ResponseWriter, r *http.Request) {
 
 func FinddogsByStatus(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		fmt.Fprintf(w, "Hello World!")
+		m := Dog{sillyname.GenerateStupidName(), "Hello", int32(time.Now().Unix())}
+		b, err := json.Marshal(m)
+		if err != nil {
+      panic(err)
+    }
+		fmt.Fprintf(w, string(b))
 		w.WriteHeader(http.StatusOK)
 }
 
 func FinddogsByTags(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		m := Dog{sillyname.GenerateStupidName(), "Hello", int32(time.Now().Unix())}
+		b, err := json.Marshal(m)
+		if err != nil {
+      panic(err)
+    }
+		fmt.Fprintf(w, string(b))
 		w.WriteHeader(http.StatusOK)
 }
 
 func GetdogById(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		m := Dog{sillyname.GenerateStupidName(), "Hello", int32(time.Now().Unix())}
+		b, err := json.Marshal(m)
+		if err != nil {
+      panic(err)
+    }
+		fmt.Fprintf(w, string(b))
 		w.WriteHeader(http.StatusOK)
 }
 
 func Updatedog(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		m := Dog{sillyname.GenerateStupidName(), "Hello", int32(time.Now().Unix())}
+		b, err := json.Marshal(m)
+		if err != nil {
+      panic(err)
+    }
+		fmt.Fprintf(w, string(b))
 		w.WriteHeader(http.StatusOK)
 }
 
 func UpdatedogWithForm(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		m := Dog{sillyname.GenerateStupidName(), "Hello", int32(time.Now().Unix())}
+		b, err := json.Marshal(m)
+		if err != nil {
+      panic(err)
+    }
+		fmt.Fprintf(w, string(b))
 		w.WriteHeader(http.StatusOK)
 }
 
 func UploadFile(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		m := Dog{sillyname.GenerateStupidName(), "Hello", int32(time.Now().Unix())}
+		b, err := json.Marshal(m)
+		if err != nil {
+      panic(err)
+    }
+		fmt.Fprintf(w, string(b))
 		w.WriteHeader(http.StatusOK)
 }
 
